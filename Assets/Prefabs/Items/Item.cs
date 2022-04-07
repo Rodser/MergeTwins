208e604665ad9e52a014130409d50b610accf3d9
Sergey <rodser@ro.ru>
2022-04-07 21:24:34 +0400

    Drag Items

--- Assets/Prefabs/Items/Item.cs
+++ Assets/Prefabs/Items/Item.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace MiniIT.Test.Items
 {
     [CreateAssetMenu(fileName ="Item", menuName ="Game/Item", order = 2)]
-    public class Item : ScriptableObject, IItem
+    public class Item : ScriptableObject
     {
         [SerializeField] private int level = 1;
-        [SerializeField] private GameObject prefab = null;
+        [SerializeField] private ItemView prefab = null;
 
         public int Level => level;
-        public GameObject Prefab => prefab;
+        public ItemView Prefab => prefab;
         
         
         public void Spawn(Vector3 position)
---
+++ Assets/Prefabs/Items/ItemView.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace MiniIT.Test.Items
+{
+    public class ItemView : MonoBehaviour, IItem
+    {
+        private Vector3 startPosition;
+        private Vector3 offset;
+        private Vector3 coord;
+
+        private void OnMouseDown()
+        {
+            startPosition = transform.position;
+            coord = Camera.main.WorldToScreenPoint(transform.position);
+            offset = transform.position - GetMouseWorldPosition();
+        }
+
+        private void OnMouseDrag()
+        {
+            transform.position = GetMouseWorldPosition() + offset;
+            Debug.Log($"x = {transform.position.x}, y = {transform.position.y}, z = {transform.position.z}");
+        }
+
+        private void OnMouseUp()
+        {
+            transform.position = startPosition;
+        }
+
+        private Vector3 GetMouseWorldPosition()
+        {
+            Vector3 mousePoint = Input.mousePosition;
+            mousePoint.z = coord.z;
+            Debug.Log($"x = {mousePoint.x}, y = {mousePoint.y}, z = {mousePoint.z}");
+            return Camera.main.ScreenToWorldPoint(mousePoint);
+        }
+    }
+}
