596d53fd5e3696cbc29ab21528be5ec4a1641fd3
Sergey <rodser@ro.ru>
2022-04-11 10:05:49 +0400

    Load first scene from menu

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -2,11 +2,19 @@
 {
     public static class Game
     {
+        private static int level = 1;
+        
         public static LevelManager LevelManager { get; set; }
-
+        public static int Level => level;
+        
         public static void SetLevelManager(LevelManager levelManager)
         {
             LevelManager = levelManager;
+        }
+
+        public static void SetLevel(int newLevel)
+        {
+            level = newLevel;
         }
     }
 }
--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -1,14 +1,41 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace MiniIT.Test.UI
 {
     public class Menu : MonoBehaviour
     {
+        [SerializeField] private GameObject sceneUI;
         [SerializeField] private Image panelMenu = null;
         [SerializeField] private Button settingButton = null;
         [SerializeField] private Button playButton = null;
         [SerializeField] private Button exitButton = null;
 
+        private void Start()
+        {
+            this.settingButton.onClick.AddListener(OpenSetting);
+            this.playButton.onClick.AddListener(LoadScene);
+            this.exitButton.onClick.AddListener(Quit);
+        }
+
+        private void OpenSetting()
+        {
+            Debug.Log("Setting");
+        }
+
+        private void LoadScene()
+        {
+            Debug.Log("Load Scene");
+            DontDestroyOnLoad(sceneUI);
+            SceneManager.LoadSceneAsync(Game.Level);
+            this.gameObject.SetActive(false);
+        }
+
+        private void Quit()
+        {
+            Debug.Log("Quit application!");
+            Application.Quit();
+        }
     }
 }
